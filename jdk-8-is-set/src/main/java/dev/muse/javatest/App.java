d276aab57b7239265bfe91b170509a06f8901684
Amelia Keibler <akeibler@sonatype.com>
2023-01-09 15:37:33 -0500

    Initial synthetic test data

---
+++ jdk-8-is-set/src/main/java/dev/muse/javatest/App.java
@@ -0,0 +1,13 @@
+package dev.muse.javatest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        new RuntimeException();
+    }
+}
---
+++ rust-example/target/debug/deps/rust_example-33615d3fd437af21.d
@@ -0,0 +1,7 @@
+/home/amy/code/example_repos/lift-local-tests/rust-example/target/debug/deps/rust_example-33615d3fd437af21.rmeta: src/main.rs
+
+/home/amy/code/example_repos/lift-local-tests/rust-example/target/debug/deps/rust_example-33615d3fd437af21.d: src/main.rs
+
+src/main.rs:
+
+# env-dep:CLIPPY_ARGS=
