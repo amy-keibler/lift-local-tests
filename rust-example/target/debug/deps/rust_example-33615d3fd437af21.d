ea2274a0454fd48a08e353bb4b13e73949753ba6
Amelia Keibler <akeibler@sonatype.com>
2023-01-09 15:52:09 -0500

    Whoops

--- rust-example/target/debug/deps/rust_example-33615d3fd437af21.d
+++
@@ -1,7 +0,0 @@
-/home/amy/code/example_repos/lift-local-tests/rust-example/target/debug/deps/rust_example-33615d3fd437af21.rmeta: src/main.rs
-
-/home/amy/code/example_repos/lift-local-tests/rust-example/target/debug/deps/rust_example-33615d3fd437af21.d: src/main.rs
-
-src/main.rs:
-
-# env-dep:CLIPPY_ARGS=
